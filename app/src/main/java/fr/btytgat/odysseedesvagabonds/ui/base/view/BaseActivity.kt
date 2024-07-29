33e8852713f668d198a79a1cafc66047ab5f7dfa
jubidatiloki <benjamin.tytgat@hotmail.fr>
2024-07-29 13:33:30 +0200

    UPDATE: base theme, colors and icons

--- app/src/main/java/fr/btytgat/odysseedesvagabonds/ui/base/view/BaseActivity.kt
+++ app/src/main/java/fr/btytgat/odysseedesvagabonds/ui/base/view/BaseActivity.kt
@@ -78,6 +78,7 @@ open class BaseActivity: AppCompatActivity(), IBaseView.IActivity,
     override fun setNavigationViewMenu(menu: Int) {
         try {
             navigationView.menu.clear()
+            navigationView.setBackgroundColor(R.color.colorPrimary)
             navigationView.inflateMenu(menu)
         } catch (var3: Exception) {
             var3.printStackTrace()
