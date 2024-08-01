e88e9ef17c18379eb628b3c85b6ae91ddea37c92
jubidatiloki <benjamin.tytgat@hotmail.fr>
2024-08-01 07:26:05 +0200

    WIP: character creation

---
+++ app/src/main/java/fr/btytgat/odysseedesvagabonds/ui/character/view/CharacterThirdFragment.kt
@@ -0,0 +1,19 @@
+package fr.btytgat.odysseedesvagabonds.ui.character.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fr.btytgat.odysseedesvagabonds.R
+
+class CharacterThirdFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_character_third, container, false)
+    }
+}
